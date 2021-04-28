c755e9d327e110e5825442f6cc281872e5b51b3c
Marta Skrzypczak <mart51w7@stud.kea.dk>
2021-04-28 21:57:06 +0200

    newsletter popup works

---
+++ popup.js
@@ -0,0 +1,5 @@
+document.querySelector(".x-sign").addEventListener("click", closeNewsletter);
+
+function closeNewsletter() {
+  document.querySelector(".newsletter-popup").classList.add("hidden");
+}
