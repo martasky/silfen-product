0a844b9ff7eb31ca89ffe75cab930d32aa0e8c85
Marta Skrzypczak <mart51w7@stud.kea.dk>
2021-04-29 18:16:36 +0200

    make it dynamic

---
+++ catalogue.js
@@ -0,0 +1,60 @@
+fetch("https://kea21-4d62.restdb.io/rest/silfenproducts", {
+  method: "GET",
+  headers: {
+    "x-apikey": "602e36c15ad3610fb5bb62b8",
+  },
+})
+  .then((res) => res.json())
+  .then((response) => {
+    console.log(response);
+    showProducts(response);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+function showProducts(data) {
+  console.log(data);
+
+  data.forEach((product) => {
+    console.log(product);
+
+    showProduct(product);
+  });
+}
+function showProduct(product) {
+  const tempProd = document.querySelector("template").content;
+  const clone = tempProd.cloneNode(true);
+
+  clone.querySelector(".product-name").textContent = product.name;
+  clone.querySelector(".product-image").src = product.images[1];
+
+  // clone.querySelector(".colors").textContent = product.colors[0].name;
+
+  clone.querySelector(".product-price").textContent = `${product.price} DKK`;
+
+  // if (product.colors.length > 1) {
+  //     clone.querySelector(".color2").textContent = product.colors[1].name;
+  //     clone.querySelector(".color")
+  //   }
+
+  var color_list = clone.querySelector(".product-colors");
+  var ul = document.createElement("ul");
+  product.colors.forEach(function (color) {
+    var li = document.createElement("li");
+    // li.textContent = color.name;
+    li.style.backgroundColor = color.value;
+    ul.appendChild(li);
+  });
+  color_list.appendChild(ul);
+
+  //   btnEl = clone.querySelector("button");
+  //   // btnEl.dataset.id += product._id;
+
+  //   btnEl.addEventListener("click", () => {
+  //     cart.add(product);
+  //   });
+
+  const prod = document.querySelector("#catalogue-products");
+  prod.appendChild(clone);
+}
