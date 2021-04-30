f3b976e8ff225f00baa35454e68e3ed5bf271bb6
Esther <esth2577@stud.kea.dk>
2021-04-30 09:12:01 +0200

    js and css for top menu

--- burger-menu.js
+++ burger-menu.js
@@ -1,5 +1,8 @@
 screensize = window.screen.width;
 
+menuActive();
+
+function menuActive() {
 
 if (screensize <= 1100){
 let burgermenu = 0;
@@ -49,3 +52,27 @@ function displaySecondDrop () {
 }
 }
 }}
+
+else {
+    document.querySelector("#shop").addEventListener("click", displaySecondDrop);
+    document.querySelector("#aboutdrop").addEventListener("click", displaySecondDropAbout);
+
+function displaySecondDropAbout () {
+    document.querySelector("ul .dropdown-content-two").classList.remove("dont-display");
+};
+
+function displaySecondDrop () {
+    document.querySelector("ul .dropdown-content").classList.remove("dont-display");
+    document.querySelector("li .drop-two").addEventListener("click", displayThirdDrop);
+    document.querySelector("li #bagsdrop").addEventListener("click", displayThirdDropBags);
+    function displayThirdDrop () {
+        document.querySelector("ul .dropdown-secondary-content").classList.remove("dont-display");
+    }
+    function displayThirdDropBags () {
+        document.querySelector("ul .dropdown-bags").classList.remove("dont-display")
+    }}
+
+}}
+
+window.addEventListener('resize', menuActive);
+
