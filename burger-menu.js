b91d9ae1fccc40a5dbc14aec0c58cd78b3ae0598
Esther <esth2577@stud.kea.dk>
2021-04-30 09:49:44 +0200

    js simplified top menu

--- burger-menu.js
+++ burger-menu.js
@@ -1,19 +1,9 @@
-screensize = window.screen.width;
 
-menuActive();
-
-function menuActive() {
-
-if (screensize <= 1100){
-let burgermenu = 0;
-console.log (screensize)
 burgerMenu();
+
 function burgerMenu() {
-if (burgermenu == 0) {
 document.querySelector(".close-burguer").addEventListener("click", displayBurguer);
 function displayBurguer () {
-    burgermenu = 1
-    console.log(burgermenu)
     document.querySelector("header ul:first-of-type").classList.remove("dont-display");
     document.querySelector(".close-burguer img").src = "assets/exit.png";
     document.querySelector("#shop").addEventListener("click", displaySecondDrop);
@@ -21,6 +11,7 @@ function displayBurguer () {
 
 function displaySecondDropAbout () {
     document.querySelector("ul .dropdown-content-two").classList.remove("dont-display");
+    document.querySelector("#shop").addEventListener("click", hideDropShop);
 };
 
 function displaySecondDrop () {
@@ -37,42 +28,16 @@ function displaySecondDrop () {
     document.querySelector(".close-burguer").addEventListener("click", closeMenu);
 
     function closeMenu () {
-        console.log ("burger closed");
         document.querySelector("header ul:first-of-type").classList.add("dont-display");
         document.querySelector(".close-burguer img").src = "assets/burger.png";
         document.querySelector("ul .dropdown-bags").classList.add("dont-display");
         document.querySelector("ul .dropdown-secondary-content").classList.add("dont-display");
         document.querySelector("ul .dropdown-content").classList.add("dont-display");
         document.querySelector("ul .dropdown-content-two").classList.add("dont-display");
-
-        burgermenu = 0
         burgerMenu ()        
     }
     
 }
 }
-}}
 
-else {
-    document.querySelector("#shop").addEventListener("click", displaySecondDrop);
-    document.querySelector("#aboutdrop").addEventListener("click", displaySecondDropAbout);
-
-function displaySecondDropAbout () {
-    document.querySelector("ul .dropdown-content-two").classList.remove("dont-display");
-};
-
-function displaySecondDrop () {
-    document.querySelector("ul .dropdown-content").classList.remove("dont-display");
-    document.querySelector("li .drop-two").addEventListener("click", displayThirdDrop);
-    document.querySelector("li #bagsdrop").addEventListener("click", displayThirdDropBags);
-    function displayThirdDrop () {
-        document.querySelector("ul .dropdown-secondary-content").classList.remove("dont-display");
-    }
-    function displayThirdDropBags () {
-        document.querySelector("ul .dropdown-bags").classList.remove("dont-display")
-    }}
-
-}}
-
-window.addEventListener('resize', menuActive);
 
